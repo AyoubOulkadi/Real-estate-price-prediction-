3eba3b6f9d3ce38e3b1f79fe6c464f672578e41c
ayouboul <ayoub.oulkadi@idemia.com>
2025-03-30 14:37:11 +0000

    collection commit

--- config/utils.py
+++ config/utils.py
@@ -1,6 +1,6 @@
 class IngestionHelper:
-    AWS_ACCESS_KEY_ID = 'AKIA5BPWVDAEWQ3CIC5X'
-    AWS_SECRET_ACCESS_KEY ='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'
+    AWS_ACCESS_KEY_ID = ''
+    AWS_SECRET_ACCESS_KEY =''
     bucket_name = 'sarouty-bucket'
     folder_path = '/Data/raw_Data/'
     target_folder = "raw_data"
