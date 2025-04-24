b9ca4bbe62af39ad7f7a3fc058b90c909f348136
ayouboul <ayoub.oulkadi@idemia.com>
2025-04-24 09:37:39 +0100

    Ingestion commit

--- config/utils.py
+++
@@ -1,6 +0,0 @@
-class IngestionHelper:
-
-    bucket_name = 'sarouty-bucket'
-    folder_path = '/Data/raw_Data/'
-    target_folder = "raw_data"
-
--- src/data_ingestion/ingestion.py
+++ src/data_ingestion/ingestion.py
@@ -1,28 +1,62 @@
 import boto3
 import os
 import logging
-
-from config.utils import IngestionHelper
+import argparse
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-s3 = boto3.client('s3',
-                  aws_access_key_id=IngestionHelper.AWS_ACCESS_KEY_ID,
-                  aws_secret_access_key=IngestionHelper.AWS_SECRET_ACCESS_KEY)
+
+def parse_parameters():
+    """
+    Parses parameters from the command line.
+
+    Returns
+    -------
+    tuple
+      (bucket_name, source, selected_params)
+    """
+
+    cmd_arg_parser = argparse.ArgumentParser(allow_abbrev=False)
+
+    cmd_arg_parser.add_argument('--bucket-name', type=str, required=True,
+                                help='Bucket name to store files in S3')
+    cmd_arg_parser.add_argument('--folder-path', type=str, required=True,
+                                help='Actual Path within the data exists')
+    cmd_arg_parser.add_argument('--target-folder', type=str, required=True,
+                                help='folder to store the daily data')
+
+    args = cmd_arg_parser.parse_args()
+    return args
 
 
-def transfer_files_S3(bucket_name, folder_path, target_folder):
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.csv'):
-            file_path = os.path.join(folder_path, filename)
-            file_key = f"{target_folder}/{filename}"
+def get_credentials():
+    AWS_ACCESS_KEY_ID = ""
+    AWS_SECRET_ACCESS_KEY = ""
+    return AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+
+def transfer_files_S3(s3, bucket_name, folder_path, target_folder):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            local_path = os.path.join(root, file)
+            relative_path = os.path.relpath(local_path, folder_path)
+            s3_key = os.path.join(target_folder, relative_path).replace("\\", "/")
+
             try:
-                s3.upload_file(file_path, bucket_name, file_key)
-                logging.info(f"Uploaded {file_path} to S3 bucket {bucket_name} as {file_key}")
+                s3.upload_file(local_path, bucket_name, s3_key)
+                logging.info(f"Uploaded {local_path} to S3 bucket {bucket_name} as {s3_key}")
             except Exception as e:
-                logging.error(f"An error occurred during uploading file to S3: {str(e)}")
-                raise Exception(f"An error occurred during uploading file to S3: {str(e)}")
+                logging.error(f"Error uploading {local_path} to S3: {str(e)}")
+                raise Exception(f"Error uploading {local_path} to S3: {str(e)}")
 
 
 if __name__ == "__main__":
-    transfer_files_S3(IngestionHelper.bucket_name, IngestionHelper.folder_path, IngestionHelper.target_folder)
+    args = parse_parameters()
+    aws_access_key, aws_secret_key = get_credentials()
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key
+    )
+    transfer_files_S3(s3, args.bucket_name, args.folder_path, args.target_folder)
