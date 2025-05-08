44cf5e352c06f55db9ba1ac43c11e56c232d475d
ayouboul <ayoub.oulkadi@idemia.com>
2025-05-08 15:16:40 +0100

    first commit

--- config/dags/saouty_dag.py
+++ config/dags/saouty_dag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from src.data_processing.processing_data import run_processing_pipeline
-from src.data_ingestion.ingestion import transfer_files_S3
+from src.data_ingestion.ingestion import create_databases, ingest_data
 
 default_args = {
     'owner': 'airflow',
@@ -19,29 +19,19 @@ with DAG(
         catchup=False,
         description='Process Sarouty data and upload to S3',
 ) as dag:
-    upload_raw_data = PythonOperator(
-        task_id='upload_raw_data_to_s3',
-        python_callable=transfer_files_S3,
-        op_kwargs={
-            'bucket_name': 'sarouty-bucket',
-            'folder_path': '/opt/airflow/raw_data',
-            'target_folder': 'raw_data'
-        }
-    )
-
     process_data_task = PythonOperator(
         task_id='process_sarouty_data',
         python_callable=run_processing_pipeline
     )
 
-    upload_processed_data = PythonOperator(
-        task_id='upload_processed_data_to_s3',
-        python_callable=transfer_files_S3,
-        op_kwargs={
-            'bucket_name': 'sarouty-bucket',
-            'folder_path': '/opt/airflow/processed_data',
-            'target_folder': 'processed_data'
-        }
+    create_dbs = PythonOperator(
+        task_id='create_databases',
+        python_callable=create_databases
     )
 
-    upload_raw_data >> process_data_task >> upload_processed_data
+    ingest_data = PythonOperator(
+        task_id='ingest_data_to_mysql',
+        python_callable=ingest_data
+    )
+
+    process_data_task >> create_dbs >> ingest_data
